9ea5541a3da2eaa2887746a86ab47aaba37d0d1d
Coding With Sourav <so34ur56@gmail.com>
2025-02-14 18:26:09 +0530

    update changes

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const bodyParser = require("body-parser");
 const fs = require("fs");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -13,20 +14,25 @@ app.get("/", (req, res) => {
     res.send("Backend is running!");
 });
 
+
 app.post("/submit", (req, res) => {
     const { name, email, phone, country } = req.body;
 
+    
     if (!name || !email || !phone || !country) {
         return res.status(400).json({ message: "All fields are required" });
     }
 
+   
     const formData = { name, email, phone, country, timestamp: new Date() };
+
     fs.readFile("submissions.json", (err, data) => {
         let submissions = [];
-        if (!err) {
+        if (!err && data.length) {
             submissions = JSON.parse(data);
         }
         submissions.push(formData);
+
         fs.writeFile("submissions.json", JSON.stringify(submissions, null, 2), (err) => {
             if (err) {
                 return res.status(500).json({ message: "Error saving data" });
@@ -38,5 +44,5 @@ app.post("/submit", (req, res) => {
 
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
